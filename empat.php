b5d94bdef14acdfd7a77ddab3737c731f1c30d65
ElgaFirmantara <h1101231004@student.untan.ac.id>
2024-09-04 21:14:10 +0700

    Praktikum 4D - Membuat Class dari Class Diagram

---
+++ empat.php
@@ -0,0 +1,102 @@
+<?php
+
+class Author {
+    public $name;
+    public $deskripsi;
+    public function show($type) {
+        $result = "";
+        if ($type === 'nama') {
+            $result = "Nama: " . $this->name;
+        } elseif ($type === 'deskripsi') {
+            $result = "Deskripsi: " . $this->deskripsi;
+        } elseif ($type === 'all') {
+            $result = "Nama: " . $this->name . "\nDeskripsi: " . $this->deskripsi;
+        }
+
+        return $result;
+    }
+}
+
+class Book {
+    public $ISBN;
+    public $title;
+    public $description;
+    public $category;
+    public $language;
+    public $numberOfPage;
+    public $author;
+    public $publisher;
+
+    public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher) {
+        $this->ISBN = $ISBN;
+        $this->title = $title;
+        $this->description = $description;
+        $this->category = $category;
+        $this->language = $language;
+        $this->numberOfPage = $numberOfPage;
+        $this->author = $author;
+        $this->publisher = $publisher;
+    }
+
+    public function showAll() {
+        $result = "ISBN: " . $this->ISBN . "\n";
+        $result .= "Title: " . $this->title . "\n";
+        $result .= "Description: " . $this->description . "\n";
+        $result .= "Category: " . $this->category . "\n";
+        $result .= "Language: " . $this->language . "\n";
+        $result .= "Number of Pages: " . $this->numberOfPage . "\n";
+        $result .= "Author: " . $this->author ."\n";
+        $result .= "Publisher: " . $this->publisher . "\n";
+
+        return $result;
+    }
+
+    public function detail($ISBN) {
+        if ($this->ISBN == $ISBN) {
+            return $this->showAll();
+        } else {
+            return "Book not found";
+        }
+    }
+}
+class Publisher {
+    public $name;
+    public $address;
+    private $phone;
+
+    public function setPhone($phone) {
+        $this->phone = $phone;
+        }
+
+    public function getPhone() {
+        return $this->phone;
+        }
+}
+
+
+echo "--Class Author-- \n";
+$hasil = new Author();
+$hasil -> name = 'Elga';
+$hasil -> deskripsi = 'Berkuliah di Universitas Tanjung Pura'; 
+echo "-- Pemanggilan berdasarkan nama -- \n";
+echo $hasil->show('nama') . "\n"; 
+echo "-- Pemanggilan berdasarkan Deskripsi -- \n";
+echo $hasil->show('deskripsi') . "\n"; 
+echo "-- Pemanggilan berdasarkan All -- \n";
+echo $hasil->show('all') . "\n"; 
+
+echo "--Class Book-- \n";
+$buku = new Book(000, "Pemrograman Berorientasi Objek", "Sebuah buku tentang materi pemrograman ", "Pemrograman", "Bahasa Indonesia", 27, "Elga", "Tara");
+echo "--Function ShowAll-- \n";
+echo $buku -> showAll();
+echo "--Function detail-- \n";
+echo $buku -> detail(000);
+
+echo "--Class Publisher-- \n";
+$publis = new Publisher();
+$publis -> name = "Fuad \n";
+$publis -> address = "jl. Tabrani Ahmad\n";
+$publis -> setPhone('085895675549');
+echo $publis -> getphone();
+
+?>
