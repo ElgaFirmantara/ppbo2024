e701fe6b032cfbc2aa37e6746313283034e2151c
ElgaFirmantara <h1101231004@student.untan.ac.id>
2024-09-06 16:02:20 +0700

    “Praktikum 5B sampai 5E Penggunaan Require dan Include”

--- empat.php
+++ empat.php
@@ -3,7 +3,7 @@ require_once 'Author.php';
 require_once 'Book.php';
 require_once 'Publisher.php';
 
-echo "--Class Author-- \n";
+echo "-Class Author- \n";
 $hasil = new Author();
 $hasil -> name = 'Elga';
 $hasil -> deskripsi = 'Berkuliah di Universitas Tanjung Pura'; 
@@ -15,14 +15,14 @@ echo "-- Pemanggilan berdasarkan All -- \n";
 print_r ($hasil->show('all'));
 echo "\n";
 
-echo "--Class Book-- \n";
+echo "-Class Book- \n";
 $buku = new Book(000, "Pemrograman Berorientasi Objek", "Sebuah buku tentang materi pemrograman ", "Pemrograman", "Bahasa Indonesia", 27, "Elga", "Tara");
 echo "--Function ShowAll-- \n";
 echo $buku -> showAll();
 echo "--Function detail-- \n";
 echo $buku -> detail(000);
 
-echo "--Class Publisher-- \n";
+echo "-Class Publisher- \n";
 $publis = new Publisher();
 $publis -> name = "Fuad \n";
 $publis -> address = "jl. Tabrani Ahmad\n";
