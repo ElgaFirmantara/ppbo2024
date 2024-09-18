eff98fbda07797a2bd2c6638e987b0f4238475d2
ElgaFirmantara <h1101231004@student.untan.ac.id>
2024-09-18 23:58:01 +0700

    coab-coba

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elgafirmantara/ppbo2024',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3ae87a74754d6848f23b0c64d91ad004f2e04eed',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elgafirmantara/ppbo2024' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3ae87a74754d6848f23b0c64d91ad004f2e04eed',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
