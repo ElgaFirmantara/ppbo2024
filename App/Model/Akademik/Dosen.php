c4ff1e9ab54ad41efe9a36cd2a7e8a87e1adbfd8
ElgaFirmantara <h1101231004@student.untan.ac.id>
2024-09-26 21:28:40 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- App/Model/Akademik/Dosen.php
+++ App/Model/Akademik/Dosen.php
@@ -3,8 +3,12 @@
 namespace App\Model\Akademik;
 
 class Dosen extends Pegawai {
-    public string $nidn;
-
+    private string $nidn;
+    public function __construct(int $nip, string $nama, int $no_hp, string $alamat, string $nidn)
+    {
+        parent::__construct($nip, $nama, $no_hp, $alamat);
+        $this->nidn = $nidn;
+    }
     public function mengajar(): void {
         echo "{$this->nama} sedang mengajar perkuliahan";
     }
--- App/Model/Akademik/Pegawai.php
+++ App/Model/Akademik/Pegawai.php
@@ -8,6 +8,14 @@ class Pegawai {
     protected int $no_hp;
     public string $alamat;
 
+    public function __construct(int $nip, string $nama, string $no_hp, string $alamat)
+    {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
     public function cekIn(): bool {
         return true;
     }
@@ -16,7 +24,7 @@ class Pegawai {
         return true;
     }
 
-    public function getNoHp(): int {
+    protected function getNoHp(): int {
         return $this->no_hp;
     }
 
